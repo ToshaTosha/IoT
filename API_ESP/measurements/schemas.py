96b574e2520a44318f365a25393e0656654a3bbb
злата <118795314+dzhoshua@users.noreply.github.com>
2024-03-20 14:56:15 +0800

    add schemas

---
+++ API_ESP/measurements/schemas.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+class TemperatureValue(BaseModel):
+    value: float 
+    location: str
+    device_id: str
+    timestamp: datetime
