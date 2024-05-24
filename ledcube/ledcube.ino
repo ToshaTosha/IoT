ea3c175a270ed7528626188ed874565be5e3de7b
злата <118795314+dzhoshua@users.noreply.github.com>
2024-05-24 12:15:49 +0800

    Update ledcube.ino

--- ledcube/ledcube.ino
+++ ledcube/ledcube.ino
@@ -1,14 +1,21 @@
 int rows[] = {2, 3};
 int columns[] = {4, 5, 6, 7};
 
+int size = sizeof(rows) / sizeof(rows[0]);
+int delay_time=700;
+
 void setup() {
   Serial.begin(9600);
   for (int i=2; i <= 7; i++) {
     pinMode(i, OUTPUT);
   }
-  int delay_time=200;
-  int size = sizeof(rows) / sizeof(rows[0]);
-  switch_leds(size, delay_time);
+  
+  switch_leds();
+}
+
+void rows_off(){
+  digitalWrite(rows[0], LOW);
+  digitalWrite(rows[1], LOW);
 }
 
 void columns_on(){
@@ -21,10 +28,10 @@ void columns_off(){
     digitalWrite(columns[j], LOW);
   }
 }
-void switch_leds(int size, int delay_time){
-  //1 слой 1 светодиод
-  for(int i = 0; i < size; i++){
-    digitalWrite(rows[i], HIGH);
+
+void one_led(int state1, int state2){
+    digitalWrite(rows[0], state1);
+    digitalWrite(rows[1], state2);
     for(int j = 0; j < size*2; j++){
       digitalWrite(columns[0], j == 0 ? LOW:HIGH);
       digitalWrite(columns[1], j == 1 ? LOW:HIGH);
@@ -32,13 +39,14 @@ void switch_leds(int size, int delay_time){
       digitalWrite(columns[3], j == 3 ? LOW:HIGH);
       delay(delay_time);
     }
-    digitalWrite(rows[i], LOW);    
-  }
-  columns_on();
+    rows_off();
+}
+
+void two_leds(int state1, int state2){
   
-  //по 2, 1 слой
-  for (int i = 0; i < size; i++) {
-    digitalWrite(rows[i], HIGH);
+  // ДОБАВИТЬ ДИАГОНАЛЬ///
+    digitalWrite(rows[0], state1);
+    digitalWrite(rows[1], state2);
     for(int j = 0; j < size*2; j++){
       int j2=0;
       if(j+1 != size*2){
@@ -50,36 +58,52 @@ void switch_leds(int size, int delay_time){
       digitalWrite(columns[j],HIGH);
       digitalWrite(columns[j2],HIGH);
     }
-    
-    digitalWrite(rows[i], LOW);
-  }
-  columns_on();
-  //по 3, 1 слой
-  for (int r = 0; r < size; r++) {
-    for (int c = 0; c < size*2; c++) {
-      for (int i = 0; i < size; i++) {
-        digitalWrite(rows[i], r == i ? HIGH:LOW);
-      }
+    rows_off();
+}
+
+void three_leds(int state1, int state2){
+	for (int c = 0; c < size*2; c++) {
+      digitalWrite(rows[0], state1);
+      digitalWrite(rows[1], state2);
+      
       for (int j = 0; j < size*2; j++) {
         digitalWrite(columns[j], c == j ? HIGH:LOW);
       }
       delay(delay_time);
-      columns_off();
+      rows_off();
     }
-  }
-  
-  
-  //все
-   digitalWrite(rows[0], HIGH);
-   digitalWrite(rows[1], LOW);
-   delay(delay_time);
-   digitalWrite(rows[1], HIGH);
-   digitalWrite(rows[0], LOW);
-   delay(delay_time);
-   digitalWrite(rows[0], HIGH);
 }
 
-void all_combinations(){
+void all(int state1, int state2){
+   digitalWrite(rows[0], state1);
+   digitalWrite(rows[1], state2);
+   delay(delay_time);
+   rows_off();
+}
+
+void switch_leds(){
+  //1
+  one_led(1,0);
+  one_led(0,1);
+  one_led(1,1);
+  columns_on();
+  
+  //2
+  two_leds(1,0);
+  two_leds(0,1);
+  two_leds(1,1);
+  columns_on();
+  
+  //3
+  three_leds(1,0);
+  three_leds(0,1);
+  three_leds(1,1);
+  columns_off();
+  
+  //4
+  all(1,0);
+  all(0,1);
+  all(1,1);
   
 }
 
