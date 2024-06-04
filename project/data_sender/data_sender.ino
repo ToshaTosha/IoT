766825a40a40ae4088ac8dae04701bc568c3a238
злата <118795314+dzhoshua@users.noreply.github.com>
2024-06-04 20:09:02 +0800

    reading and sending data from sds011

--- project/data_sender/data_sender.ino
+++ project/data_sender/data_sender.ino
@@ -1,4 +1,5 @@
-#include <SDS011.h>
+#include <SoftwareSerial.h>
+
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
@@ -14,23 +15,22 @@
 
 ESP8266WiFiMulti WiFiMulti;
 
-SDS011 my_sds;
+//SDS011 sds;
+SoftwareSerial sds(0, 2); // RX, TX
 Adafruit_BME280 bme; 
 
 const char* CLI_SSID = "me";
 const char* CLI_PASS = "123456780";
 
-//float p10, p25; //sds011
-//int error;
+int pm10, pm25; //sds011
 
 float temperature, pressure, altitude, humidity; //bme280
 
 unsigned long delayTime;
 
 void setup() {
-    //my_sds.begin(D3, D4); //RX, TX
+    sds.begin(9600);
     Serial.begin(9600);
-    Serial.println("START");
 
     for (uint8_t t = 4; t > 0; t--) {
       Serial.printf("[SETUP] WAIT %d...\n", t);
@@ -40,16 +40,9 @@ void setup() {
     WiFi.mode(WIFI_STA);
     WiFiMulti.addAP(CLI_SSID, CLI_PASS);
 
-    unsigned status;
-    status = bme.begin(0x76);  
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
-        while (1) delay(10);
+    if (!bme.begin(0x76)) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+      while (1);
     }
     delayTime = 10000;
 
@@ -62,10 +55,14 @@ void loop() {
     WiFiClient client;
     HTTPClient http;
 
-    //POST
+    read_sds011();
     read_bme280();
 
+   //POST
     StaticJsonDocument<200> doc;
+    doc["pm10"] = pm10;
+    doc["pm25"] = pm25;
+
     doc["temperature"] = temperature;
     doc["pressure"] = pressure;
     doc["altitude"] = altitude;
@@ -76,20 +73,15 @@ void loop() {
     Serial.print("serialized json: ");
     Serial.println(serialized_json);
 
-    // configure traged server and url
     http.begin(client, "http://192.168.10.48:8005/items");
     http.addHeader("Content-Type", "application/json");
 
     Serial.print("[HTTP] POST...\n");
-    // start connection and send HTTP header and body
     int httpCode = http.POST(serialized_json);
 
-    // httpCode will be negative on error
-    if (httpCode > 0) {
-      // HTTP header has been send and Server response header has been handled
+    if (httpCode > 0) {s
       Serial.printf("[HTTP] POST... code: %d\n", httpCode);
 
-      // file found at server
       if (httpCode == HTTP_CODE_OK) {
         const String& payload = http.getString();
         Serial.println("received payload:\n<<");
@@ -104,7 +96,24 @@ void loop() {
   delay(delayTime);
 }
 
+void read_sds011(){
+  
+  if (sds.available()) {
+    byte buffer[10];
+    sds.readBytes(buffer, 10);
 
+    pm25 = (buffer[3] * 256 + buffer[2]);
+    pm10 = (buffer[5] * 256 + buffer[4]);
+    
+    Serial.print("PM2.5: ");
+    Serial.print(pm25);
+    Serial.print(" PM10: ");
+    Serial.println(pm10);
+  }
+  else{
+    Serial.println("sds not available");
+  }
+}
 void read_bme280() {
     temperature = bme.readTemperature();
     Serial.print("Temperature = ");
@@ -129,26 +138,3 @@ void read_bme280() {
     Serial.println();
 }
 
-// void loop(){
-  
-  
-  // if (sds.available()) {
-  //   byte buffer[10];
-  //   sds.readBytes(buffer, 10);
-
-  //   // Считываем данные PM2.5 и PM10
-  //   unsigned int pm25 = (buffer[3] * 256 + buffer[2]);
-  //   unsigned int pm10 = (buffer[5] * 256 + buffer[4]);
-
-  //   // Выводим данные в монитор последовательного порта
-  //   Serial.print("PM2.5: ");
-  //   Serial.print(pm25);
-  //   Serial.println(" PM10: ");
-  //   Serial.println(pm10);
-  // }
-  // else{
-  //   Serial.println("sds not available");
-  // }
-  // delay(1000);
-  
-//}
