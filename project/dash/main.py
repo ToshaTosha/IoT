fc4205dabd3e9d74cc5a32f2c46c8da52ecffec3
unknown <veronikatrubina2121@gmail.com>
2024-06-06 17:56:45 +0300

    edit dash

--- project/dash/main.py
+++ project/dash/main.py
@@ -2,27 +2,62 @@ import dash
 from dash import dcc, html
 import plotly.express as px
 import pandas as pd
-import requests
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Float, DateTime
+from datetime import datetime
 
-df = pd.DataFrame(requests.get('http://server:8000/data').json())
+# Подключение к базе данных
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Определение модели
+Base = declarative_base()
+class Item(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True, index=True)
+    pm10 = Column(Integer, index=True)
+    pm25 = Column(Integer, index=True)
+    temperature = Column(Float, index=True)
+    pressure = Column(Float, index=True)
+    altitude = Column(Float, index=True)
+    humidity = Column(Float, index=True)
+    timestamp = Column(DateTime, default=datetime.utcnow)
 
 # Создание экземпляра Dash
-app_dash = dash.Dash(__name__)
+app = dash.Dash(__name__)
 
-# Определение макета для приложения Dash
-app_dash.layout = html.Div([
-    dcc.Graph(id='graph'),
+# Функция для обновления данных
+def update_data():
+    query = session.query(Item.timestamp, Item.temperature, Item.pressure).all()
+    df = pd.DataFrame(query, columns=['timestamp', 'temperature', 'pressure'])
+    return df
+
+# Создание графиков с использованием Dash
+app.layout = html.Div([
+    dcc.Interval(
+        id='interval-component',
+        interval=10*1000,  # в миллисекундах
+        n_intervals=0
+    ),
+    dcc.Graph(id='temperature-graph'),
+    dcc.Graph(id='pressure-graph')
 ])
 
-# Обновление графика при изменении данных
-@app_dash.callback(
-    dash.dependencies.Output('graph', 'figure'),
-    [dash.dependencies.Input('dropdown', 'value')]
+# Обновление данных и графиков
+@app.callback(
+    [dash.dependencies.Output('temperature-graph', 'figure'),
+     dash.dependencies.Output('pressure-graph', 'figure')],
+    [dash.dependencies.Input('interval-component', 'n_intervals')]
 )
-def update_graph(selected_value):
-    fig = px.scatter(df, x='name', y='description', title='График')
-    return fig
-
+def update_graphs(n):
+    df = update_data()
+    temperature_fig = px.line(df, x='timestamp', y='temperature', title='Temperature')
+    pressure_fig = px.line(df, x='timestamp', y='pressure', title='Pressure')
+    return temperature_fig, pressure_fig
 
 if __name__ == '__main__':
-    app_dash.run_server(debug=True)
+    app.run_server(debug=True)
